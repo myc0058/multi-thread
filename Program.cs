89bc8af4c7380df9642ed392085df32c28fc46c7
Brown <myc0058@gmail.com>
2023-07-06 19:10:16 +0900

    Section3

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
 ﻿
-internal class Program
+namespace MultiThread
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Process Start!");
-
-        //Thread 오브젝트 만들기
-        var thread1 = new Thread(DoWork);
-        var thread2 = new Thread(DoWork);
-
-        //Thread 시작
-        thread1.Start();
-        thread2.Start();
-
-        //Thread 종료 대기
-        thread1.Join();
-        thread2.Join();
-
-        Console.WriteLine("All done!");
-
-        static void DoWork()
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Doing work...");
-            Thread.Sleep(1000);
-            Console.WriteLine("Work done!");
+            //Section2.StartUp();       
+            Section3.StartUp();
         }
     }
 }
---
+++ src/Section3.cs
@@ -0,0 +1,40 @@
+﻿namespace MultiThread
+{
+    public static class Section3
+    {
+        private static bool shouldStop = false;
+
+        public static void StartUp() {
+            Console.WriteLine("Process Start!");
+
+            //Thread 오브젝트 만들기
+            var thread1 = new Thread(DoWork);
+            var thread2 = new Thread(DoWork);
+
+            //Thread 시작
+            thread1.Start();
+            thread2.Start();
+
+            
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            //shouldStop = true; //visibilty 문제 발생
+            Volatile.Write(ref shouldStop, true);
+            Console.WriteLine("exiting...");
+            
+            //Thread 종료 대기
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("All done!");
+        }
+        
+        private static void DoWork()
+        {
+            while(!shouldStop) {
+                Console.WriteLine("shouldStop: " + shouldStop);
+            }
+        }
+    }
+}
